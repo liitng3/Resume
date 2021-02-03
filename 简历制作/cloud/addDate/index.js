f80f3411951f42328eb2a5008eeecdd2fdaeaaca
liitng3 <898260697@qq.com>
2021-02-03 16:52:56 +0800

    1.3

---
+++ 简历制作/cloud/addDate/index.js
@@ -0,0 +1,24 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+// cloud.init()
+cloud.init({
+  env: "resume-dbcdg"
+})
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  return cloud.database().collection("user").add({
+    data: {
+      name: event.name,
+      age: event.age,
+      email: event.email,
+      face: event.face,
+      marry: event.marry,
+      nation: event.nation,
+      sex: event.sex,
+      tel: event.tel
+    }
+  })
+  
+}
--- 简历制作/pages/write/write.js
+++ 简历制作/pages/write/write.js
@@ -5,7 +5,14 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    name:"",
+    age:18,
+    email:"",
+    face:'',
+    marry:"",
+    nation:'',
+    sex:"",
+    tel:""
   },
 
   /**
@@ -62,5 +69,88 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  getInput1(e) {
+    this.setData({
+      name: e.detail.value
+    })
+  },
+  getInput2(e) {
+    this.setData({
+      age: e.detail.value
+    })
+  },
+  getInput3(e) {
+    this.setData({
+      sex: e.detail.value
+    })
+  },
+  getInput4(e) {
+    this.setData({
+      email: e.detail.value
+    })
+  },
+  getInput5(e) {
+    this.setData({
+      nation: e.detail.value
+    })
+  },
+  getInput6(e) {
+    this.setData({
+      marry: e.detail.value
+    })
+  },
+  getInput7(e) {
+    this.setData({
+      tel: e.detail.value
+    })
+  },
+  getInput8(e) {
+    this.setData({
+      face: e.detail.value
+    })
+  },
+
+  postForm(f) {
+    var that=this;
+    //邮箱验证
+    if (!(/^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*\.[a-zA-Z0-9]{2,6}$/.test(this.data.email))) {
+      wx.showToast({
+        title: '邮箱输入有误',
+        duration: 2000,
+        icon: 'none'
+      });
+      return false
+    }
+    if (!(/^((13[0-9])|(14[0-9])|(15[0-9])|(17[0-9])|(18[0-9]))\d{8}$/.test(this.data.tel))) {
+      wx.showToast({
+        title: '手机号码有误',
+        duration: 2000,
+        icon: 'none'
+      });
+      return false
+    }
+    
+    wx.cloud.callFunction({
+      name: 'addDate',
+      data: {
+        name: that.data.name,
+        age: that.data.age,
+        email: that.data.email,
+        face: that.data.face,
+        marry: that.data.marry,
+        nation: that.data.nation,
+        sex: that.data.sex,
+        tel: that.data.tel
+      }
+    }).then(res => {
+      console.log("成功", res)
+    }).catch(res => {
+      console.log("失败", res)
+    })
+
+   
   }
+
+
 })
