2e954e910ac7c9706074d15dc0440fda20ea3081
liitng3 <898260697@qq.com>
2021-02-10 08:36:26 +0800

    1.8

--- 简历制作/pages/modle1/modle1.js
+++ 简历制作/pages/modle1/modle1.js
@@ -5,14 +5,70 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    name: "",
+    age: 18,
+    email: "",
+    face: '',
+    marry: "",
+    nation: '',
+    sex: "",
+    tel: "",
+    imgUrl:"",
+    assess:'',
+    hobby:'',
+    intend:''
+  },
+  
+  save(){
+    wx.canvasToTempFilePath({
+      x: 0,
+      y: 0,
+      width: 500,
+      height: 500,
+      destWidth: 300,
+      destHeight: 600,
+      canvasId: 'myCanvas',
+      success: function (res) {
+        wx.saveImageToPhotosAlbum({
+          filePath: res.tempFilePath,
+        })
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var that = this;
+    wx.cloud.callFunction({
+      name: 'getData',
+      success: res => {
+        that.setData({
+          imgUrl:res.result.data[0].imgUrl,
+          name: res.result.data[0].name,
+          age: res.result.data[0].age,
+          email: res.result.data[0].email,
+          face: res.result.data[0].face,
+          marry: res.result.data[0].marry,
+          nation: res.result.data[0].nation,
+          sex: res.result.data[0].sex,
+          tel: res.result.data[0].tel,
+          assess: res.result.data[0].assess,
+          hobby: res.result.data[0].hobby,
+          intend: res.result.data[0].intend
+        })
+        const ctx = wx.createCanvasContext('myCanvas');
+        ctx.setFillStyle('red')
+        ctx.fillRect(20, 20, 150, 100)
+        ctx.setFontSize(20)
+        ctx.fillText(this.data.name, 20, 20)
+        ctx.fillText('MINA', 100, 100)
+        ctx.draw()
+      },
+      fail: err => {
+      }
+    })
   },
 
   /**
