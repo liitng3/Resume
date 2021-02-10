2162f7c671309645d696be87dc3c005ae8e2f021
liudanqing <1278476591@qq.com>
2021-02-10 11:01:28 +0800

    1.9

--- 简历制作/pages/modle1/modle1.js
+++ 简历制作/pages/modle1/modle1.js
@@ -16,17 +16,18 @@ Page({
     imgUrl:"",
     assess:'',
     hobby:'',
-    intend:''
+    intend:'',
+    bgImgPath:''
   },
   
   save(){
     wx.canvasToTempFilePath({
       x: 0,
       y: 0,
-      width: 500,
-      height: 500,
+      width: 300,
+      height: 400,
       destWidth: 300,
-      destHeight: 600,
+      destHeight: 400,
       canvasId: 'myCanvas',
       success: function (res) {
         wx.saveImageToPhotosAlbum({
@@ -59,16 +60,56 @@ Page({
           intend: res.result.data[0].intend
         })
         const ctx = wx.createCanvasContext('myCanvas');
-        ctx.setFillStyle('red')
-        ctx.fillRect(20, 20, 150, 100)
-        ctx.setFontSize(20)
-        ctx.fillText(this.data.name, 20, 20)
-        ctx.fillText('MINA', 100, 100)
-        ctx.draw()
+        ctx.setFillStyle('#b8b4cb')
+        ctx.fillRect(0, 0, 100, 400)
+        ctx.moveTo(0, 122.5)
+        ctx.lineTo(96, 122.5)
+        ctx.setStrokeStyle('#748ca6')
+        ctx.setLineCap('round')
+        ctx.setLineWidth(10)
+        ctx.setFillStyle('#748ca6')
+        // ctx.globalAlpha=0.4;
+
+        ctx.fillRect(18, 130, 64, 18)
+        ctx.setFillStyle('black')
+        ctx.setFontSize(12)
+        ctx.fillText('个人信息',25,143)
+        ctx.setFontSize(8)
+        ctx.fillText('姓名：'+that.data.name,6,160)
+        ctx.fillText('年龄：'+that.data.age,6,170)
+        ctx.fillText('性别：'+that.data.sex,6,180)
+        ctx.fillText('民族：'+that.data.nation,6,190)
+        ctx.fillText('手机号：'+that.data.tel,6,200)
+        ctx.fillText('婚姻状况：'+that.data.marry,6,210)
+        ctx.fillText('政治面貌：'+that.data.face,6,220)
+        ctx.fillText('邮箱：'+that.data.email,6,230)
+        ctx.stroke()
+        wx.getImageInfo({
+          src: that.data.imgUrl,
+          success(res){
+            ctx.drawImage(res.path,18,18,64,86.5)
+            ctx.draw()
+          }
+        })
+        // ctx.drawImage(that.data.bgImgPath,0,0,100,150)
+        // ctx.draw()
       },
       fail: err => {
       }
     })
+
+    // wx.downloadFile({
+    //   url: 'cloud://resume-dbcdg.7265-resume-dbcdg-1304912994/1612597421452.png',//网络路径
+    //   success: function (res3) {
+    //     //背景图
+    //     that.setData({
+    //       bgImgPath: res3.tempFilePath
+    //     })
+    //     console.log(that.data.bgImgPath)
+    //   }
+    // })
+    // console.log(that.data.bgImgPath)
+    // console.log(that.data.imgUrl)
   },
 
   /**
